ef33debf7fffc991f282de017d171601dfc74898
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2025-02-11 13:41:00 +0100

    feat: Added flag field (#17)

---
+++ toolbox/commands/encrypt_flag.py
@@ -0,0 +1,22 @@
+import hashlib
+
+import rich
+import typer
+
+
+def encrypt_flag():
+    """
+    Encrypt flag via sha256 mand give it back to user.
+    Don't input whole flags, only their inner strings.
+    For example:
+    Not "hack4krak('skibidi')" but just "skibidi".
+    """
+    while True:
+        flag = typer.prompt("Input flag to encrypt")
+        flag_retype = typer.prompt("Retype flag to confirm")
+        if flag != flag_retype:
+            rich.print("\n[red]Inputs are not the same\n")
+        else:
+            encrypted_flag = hashlib.sha256(flag.encode('utf-8'))
+            print(encrypted_flag.hexdigest())
+            break
