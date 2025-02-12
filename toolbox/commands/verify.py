cb4cd3035948a53ada6ec2e09d7bc731de923e2f
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2025-02-12 21:56:19 +0100

    feat: Tasks assets (#19)
    * feat: Tasks assets
    
    * fix: Fixed issue with test by switching to more safe get method
    
    * enhancement: Made assets not required
    
    * enhancement: Moved veryfing assets to other function, handled case when there are unregistered assets
    
    * fix: Fixed bug where you could pass verification with unregistered asset if you havent registered any assets
    
    * fix: Fixed some war crimes and other things
    
    * enhancement: Added tests
    
    * chore: Ruff hates me
    
    * chore: REFORMAT @Norbiros CODE
    
    * chore: Reformat code

--- toolbox/commands/verify.py
+++ toolbox/commands/verify.py
@@ -26,6 +26,7 @@ def verify(context: typer.Context):
     for subdir_path in find_tasks(tasks_directory):
         config_path = subdir_path / 'config.yaml'
         description_path = subdir_path / 'description.md'
+        assets_path = subdir_path / 'assets'
 
         if not config_path.is_file():
             continue
@@ -35,15 +36,22 @@ def verify(context: typer.Context):
             rich.print(f"[red]Missing description file for {subdir_path}")
             continue
 
+        yaml_data = yaml.safe_load(config_path.read_text(encoding='utf-8'))
+
         try:
             # print(yaml.sa
             # fe_load(config_path.read_text(encoding='utf-8')))
-            yaml_data = yaml.safe_load(config_path.read_text(encoding='utf-8'))
             validate(yaml_data, schema)
-            valid_count += 1
         except ValidationError as error:
             invalid_count += 1
             rich.print(f"[red]Validation error in {config_path}: {error.message}")
+            continue
+
+        if not verify_assets(yaml_data, assets_path, subdir_path):
+            invalid_count += 1
+            continue
+
+        valid_count += 1
 
     total_tasks = valid_count + invalid_count
     rich.print(f"\nFinished validating all tasks: {total_tasks} tasks processed.")
@@ -53,3 +61,25 @@ def verify(context: typer.Context):
         rich.print(f"[red]{invalid_count} tasks are invalid.")
         raise Exit(code=1)
 
+
+def verify_assets(yaml_data: dict, assets_path: Path, subdir_path: Path) -> bool:
+    assets = yaml_data.get("assets", [])
+
+    config_assets_paths = [asset["path"] for asset in assets]
+    for asset in assets_path.iterdir():
+        if asset.is_file() and asset.name not in config_assets_paths:
+            rich.print(f"[red]Unregistered asset file {asset.name} for {subdir_path}")
+            return False
+    if not assets:
+        return True
+    if not assets_path.is_dir():
+        rich.print(f"[red]Missing assets directory for {subdir_path}")
+        return False
+
+    for asset in assets:
+        asset_path = assets_path / str(asset["path"])
+        if not asset_path.is_file():
+            rich.print(f"[red]Missing asset file {asset} for {subdir_path}")
+            return False
+
+    return True
--- toolbox/tests/test_verify.py
+++ toolbox/tests/test_verify.py
@@ -1,12 +1,12 @@
 import json
 from pathlib import Path
-from unittest.mock import MagicMock, mock_open, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 import yaml
 from click.exceptions import Exit
 
-from toolbox.commands.verify import verify
+from toolbox.commands.verify import verify, verify_assets
 
 
 @pytest.fixture
@@ -42,37 +42,91 @@ def invalid_task_config():
         "task_name": "Task 1"
     }
 
+@pytest.fixture
+def valid_assets():
+    return {
+        "assets": [
+            {
+                "description": "dziengiel",
+                "path": "asset1.txt",
+            },
+            {
+                "description": "dziengiel",
+                "path": "asset2.txt",
+            }
+        ]
+    }
 
 @patch.object(Path, "iterdir")
 @patch.object(Path, "is_dir")
 @patch.object(Path, "is_file")
-@patch.object(Path, "read_text", new_callable=mock_open)
-def test_verify_valid(mock_open_func, mock_isfile, mock_isdir, mock_listdir, mock_context, valid_schema, valid_task_config):
-    mock_listdir.return_value = [Path("valid_task")]
-    mock_isdir.return_value = True
-    mock_isfile.return_value = True
-    mock_open_func.side_effect = [
+@patch.object(Path, "read_text")
+@patch("toolbox.commands.verify.verify_assets")
+def test_verify_valid(
+        mock_verify_assets,
+        mock_read_text,
+        mock_is_file,
+        mock_is_dir,
+        mock_iterdir,
+        mock_context,
+        valid_schema,
+        valid_task_config
+    ):
+    mock_verify_assets.return_value = True
+    mock_iterdir.return_value = [Path("valid_task")]
+    mock_is_dir.return_value = True
+    mock_is_file.return_value = True
+    mock_read_text.side_effect = [
         json.dumps(valid_schema),
         yaml.dump(valid_task_config)
     ]
 
     verify(mock_context)
 
-    mock_open_func.assert_called()
+    mock_read_text.assert_called()
 
 
 @patch.object(Path, "iterdir")
 @patch.object(Path, "is_dir")
 @patch.object(Path, "is_file")
-@patch.object(Path, "read_text", new_callable=mock_open)
-def test_verify_invalid(mock_open_func, mock_isfile, mock_isdir, mock_listdir, mock_context, valid_schema, invalid_task_config):
-    mock_listdir.return_value = [Path("invalid_task")]
-    mock_isdir.return_value = True
-    mock_isfile.return_value = True
-    mock_open_func.side_effect = [
+@patch.object(Path, "read_text")
+def test_verify_invalid(mock_read_text, mock_is_file, mock_is_dir, mock_iterdir, mock_context, valid_schema, invalid_task_config):
+    mock_iterdir.return_value = [Path("invalid_task")]
+    mock_is_dir.return_value = True
+    mock_is_file.return_value = True
+    mock_read_text.side_effect = [
         json.dumps(valid_schema),
         yaml.dump(invalid_task_config)
     ]
 
     with pytest.raises(Exit):
         verify(mock_context)
+
+
+@patch.object(Path, "iterdir")
+@patch.object(Path, "is_file")
+@patch.object(Path, "is_dir")
+def test_verify_assets_valid(mock_is_dir, mock_is_file, mock_iterdir, valid_assets):
+    mock_iterdir.return_value = [Path("asset1.txt"), Path("asset2.txt")]
+    mock_is_file.return_value = True
+    mock_is_dir.return_value = True
+
+    assert(verify_assets(valid_assets, Path("assets"), Path("subdir_path")) is True)
+
+@patch.object(Path, "iterdir")
+@patch.object(Path, "is_dir")
+def test_verify_assets_missing_asset(mock_isdir, mock_iterdir, valid_assets):
+    mock_iterdir.return_value = [Path("asset1.txt")]
+    mock_isdir.return_value = True
+
+    assert(verify_assets(valid_assets, Path("assets"), Path("subdir_path")) is False)
+
+@patch.object(Path, "iterdir")
+@patch.object(Path, "is_dir")
+@patch.object(Path, "is_file")
+def test_verify_assets_unregistered_asset(mock_is_file, mock_is_dir, mock_iterdir, valid_assets):
+    mock_iterdir.return_value = [Path("asset1.txt"), Path("asset2.txt"), Path("asset3.txt")]
+    mock_is_file.return_value = True
+    mock_is_dir.return_value = True
+
+    assert(verify_assets(valid_assets, Path("assets"), Path("subdir_path")) is False)
