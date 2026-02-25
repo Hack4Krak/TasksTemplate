bdb997a3123c9dc2a779c1aed70c805fd7fd1348
Kubaryt <sanieliathecat@gmail.com>
2026-02-25 20:47:27 +0100

    feat: Define maximal teams for event and organization (#57)

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from typing import Literal
 
 import yaml
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+from pydantic_core import PydanticCustomError
 
 
 class YamlConfig:
@@ -24,8 +25,19 @@ class EventConfig(YamlConfig, BaseModel):
 class RegistrationConfig(YamlConfig, BaseModel):
     start_date: datetime = Field(..., alias="start-date")
     end_date: datetime = Field(..., alias="end-date")
+    max_teams: int = Field(..., alias="max-teams", ge=1)
     max_team_size: int = Field(..., alias="max-team-size", ge=1)
     registration_mode: Literal["internal", "external"] = Field(..., alias="registration-mode")
+    max_teams_per_organization: int | None = Field(default=None, alias="max-teams-per-organization", ge=1)
+
+    @model_validator(mode="after")
+    def check_max_teams_per_org_if_registration_external(self):
+        if self.registration_mode == "external" and self.max_teams_per_organization is None:
+            raise PydanticCustomError(
+                "missing_max_teams_per_organization",
+                "'max-teams-per-organization' must be provided if registration-mode is external",
+            )
+        return self
 
 
 class Label(BaseModel):
